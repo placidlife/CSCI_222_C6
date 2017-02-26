9eb3d796dca6f52b1a5248a2bf005b73f4373d8a
Mei <meikonishi@gmail.com>
2017-02-26 15:40:24 +0800

    added search class

---
+++ 3. Project codes/temp dump/Search.cpp
@@ -0,0 +1,56 @@
+#include "Search.h"
+
+Search::Search(void * WM){
+	// TODO
+}
+		
+bool Search::checkItemExists(string itemID){
+	// TODO
+}
+		
+bool Search::checkItemExists(string itemName, itemCat, itemSubCat){
+	// TODO
+}
+		
+
+void Search::searchItemName(){
+	// TODO
+}
+	
+void Search::searchItemCat(){
+	// TODO
+}
+
+void Search::searchItemPrice(){
+	// TODO
+}
+
+void Search::searchItemQuantity(){
+	// TODO
+}
+
+
+vector<StockItem *> Search::findItemID(string itemID){
+	// TODO
+}
+
+vector<StockItem *> Search::findItemName(string itemName){
+	// TODO
+}
+
+vector<StockItem *> Search::findItemCat(string itemCat, string itemSubCat){
+	// TODO
+}
+
+vector<StockItem *> Search::findItemPrice(int min, int max){
+	// TODO
+}
+
+vector<StockItem *> Search::fidnItemQuantity(int min, int max){
+	// TODO
+}
+
+void Search::printSearchResults (vector<StockItem *>){
+	// TODO
+}
+
---
+++ 3. Project codes/temp dump/Search.h
@@ -0,0 +1,26 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+class Search {
+	public: 
+		Search(void * WM);
+		
+		bool checkItemExists(string itemID);
+		bool checkItemExists(string itemName, itemCat, itemSubCat);
+		
+		void searchItemName();	
+		void searchItemCat();
+		void searchItemPrice();
+		void searchItemQuantity();
+	private: 
+		void * WM; 
+
+		vector<StockItem *> findItemID(string itemID);
+		vector<StockItem *> findItemName(string itemName);
+		vector<StockItem *> findItemCat(string itemCat, string itemSubCat);
+		vector<StockItem *> findItemPrice(int min, int max);
+		vector<StockItem *> fidnItemQuantity(int min, int max);
+		void printSearchResults (vector<StockItem *>);
+};
+
+#endif
